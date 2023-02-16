d296ee22c56554a0c03f2d79a448ec905f9a871a
MacbookAir <asyadnazeem@gmail.com>
2023-02-16 11:18:58 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+let inputEl = document.getElementById("input-el")
+let inputBtn = document.getElementById("input-btn")
+let lengthEL = document.getElementById("length-el")
+let volumeEl = document.getElementById("volume-el")
+let massEl = document.getElementById("mass-el")
+
+function length(){
+    let baseValue = inputEl.value
+    let meter = 3.281
+    let feet = 0.3048
+    lengthEL.textContent = `${baseValue} meters = ${(baseValue * feet).toFixed(3)} feet | ${baseValue} feet = ${(baseValue * meter).toFixed(3)} meters`
+}
+
+function volume(){
+    let baseValue = inputEl.value
+    let gallonToLiters = 0.264
+    let litersToGallon = 3.785
+    volumeEl.textContent = `${baseValue} liters = ${(baseValue * litersToGallon).toFixed(3)} gallon | ${baseValue} gallon = ${(baseValue * gallonToLiters).toFixed(3)} liters`
+}
+
+function mass(){
+    let baseValue = inputEl.value
+    let kilogramToPound = 2.204
+    let poundToKilogram = 0.453
+    massEl.textContent = `${baseValue} kilogram = ${(baseValue * kilogramToPound).toFixed(3)} pound | ${baseValue} pound = ${(baseValue * poundToKilogram).toFixed(3)} kilogram`
+}
+
+inputBtn.addEventListener("click", function (){
+    length()
+    volume()
+    mass()
+})
